e3aa0454f99e0980a1de42bac7f07bfb20746a52
graham <graham.annett@gmail.com>
2023-11-21 14:17:55 -0700

    commit

--- tests/external/test_compare_fuyu.py
+++ tests/external/test_compare_fuyu.py
@@ -1,7 +1,10 @@
 import unittest
 
+import torch
 from PIL import Image
 from transformers import FuyuProcessor, FuyuForCausalLM
+
+from config.fuyu import FuyuInfo
 
 MODEL_ID = "adept/fuyu-8b"  # https://huggingface.co/adept/fuyu-8b
 
@@ -19,6 +22,16 @@ class TestFuyuProcessor(unittest.TestCase):
         image = Image.open(image_path)
 
         data = processor(text=text, images=[image, image])
+
+
+class TestProcessor(unittest.TestCase):
+    def test_fuyu_processor(self):
+        processor = FuyuInfo.ProcessorCls.from_pretrained(FuyuInfo.model_name)
+
+        image = torch.rand(3, 1280, 1080)
+        text = "Task: Find JetBlue career openings in New York Previous Actions Next Action:CLICK  @ <box>172, 4215, 234, 4233</box>"
+
+        data = processor(text=text, images=image)
 
 
 class TestFuyuModel(unittest.TestCase):
