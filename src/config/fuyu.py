029b8143bed7d88b75e0d59efc31ebee1227cf8c
graham <graham.annett@gmail.com>
2023-11-25 22:38:33 -0700

    commit

--- src/config/fuyu.py
+++ src/config/fuyu.py
@@ -1,12 +1,14 @@
 import transformers
 
 from pretrain_mm.utils.config_utils import ModelInitInfo, torch_dtype_float16
+from pretrain_mm.model.fuyu import FuyuProcessor
 
 FuyuInfo = ModelInitInfo(
     model_name="adept/fuyu-8b",
     model_kwargs={**torch_dtype_float16},
     ModelCls=transformers.models.fuyu.FuyuForCausalLM,
-    ProcessorCls=transformers.models.fuyu.FuyuProcessor,
+    # alternative is transformers.models.fuyu.FuyuProcessor but ours is patched
+    ProcessorCls=FuyuProcessor,
     model_extra_info={
         "decoder_layer": transformers.models.persimmon.modeling_persimmon.PersimmonDecoderLayer,
     },
---
+++ src/pretrain_mm/model/fuyu/__init__.py
@@ -0,0 +1,2 @@
+from .fuyu_processing import FuyuProcessor
+from .patched_fuyu import gather_continuous_embeddings
