c4129c341e428203746868b7bc395d0eeffad7c4
graham <graham.annett@gmail.com>
2024-03-21 23:56:26 -0600

    commit @ 2403212356

--- src/pretrain_mm/datasets/mind2web/__init__.py
+++ src/pretrain_mm/datasets/mind2web/__init__.py
@@ -1,3 +1,3 @@
-from .mind2web import Mind2Web, Mind2WebBase, Mind2WebConfig, Mind2WebIterable
+from .mind2web import Mind2Web, Mind2WebBase, Mind2WebIterable
 from .mind2web_datatypes import ActionOp, M2WAction, M2WTrajectory, Mind2WebConfig
 from .mind2web_processor import Mind2WebEncoder, Mind2WebPretrainProcessor
