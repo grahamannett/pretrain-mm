4cb80d498e1b00c41a745ff1d25109e6ce55a03d
graham <graham.annett@gmail.com>
2024-05-10 16:58:16 -0600

    commit @ 2405101658

--- src/pretrain_mm/model/adapted/loss_adapter.py
+++ src/pretrain_mm/model/adapted/loss_adapter.py
@@ -48,8 +48,8 @@ class CLMLossAdapter(nn.Module):
         # original forward just with labels removed
         outputs = self.model_forward(**kwargs)
 
-        if self.training and (labels is not None):
-            outputs.loss = self.loss_func(outputs.logits, labels)
-            breakpoint()
+        # if self.training and (labels is not None):
+        #     outputs.loss = self.loss_func(outputs.logits, labels)
+        #     breakpoint()
         return outputs
         # return self.model_forward(**kwargs)
