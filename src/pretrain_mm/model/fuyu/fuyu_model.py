b93824b11e3782a7be48a7259c3389d019045d87
graham <graham.annett@gmail.com>
2024-05-10 14:42:02 -0600

    commit @ 2405101442

--- src/pretrain_mm/model/fuyu/fuyu_model.py
+++ src/pretrain_mm/model/fuyu/fuyu_model.py
@@ -78,14 +78,14 @@ class FuyuForCausalLM(BaseFuyuForCausalLM):
             self._forward = CLMLossAdapter(self._forward, config)
 
         # make this optional to allow for easier testing
-        self.gather_continuous_embeddings = self._gather_continuous_embeddings
+        # self.gather_continuous_embeddings = self._gather_continuous_embeddings
         # if getattr(config, "patch_gather_continuous_embeddings", True):
         #     logger.warn("Patching gather_continuous_embeddings for model as HF one may be broken")
         #     self.gather_continuous_embeddings = self._gather_continuous_embeddings
         # else:
         #     logger.warn("Not patching gather_continuous_embeddings for model. Likely will not work on 4+ GPU shard")
 
-    def _gather_continuous_embeddings(
+    def gather_continuous_embeddings(
         self,
         word_embeddings: torch.Tensor,
         continuous_embeddings: torch.Tensor,
