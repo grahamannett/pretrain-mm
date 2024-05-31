5ef5e5dae60102286e94eb4eb3c78ab8ed0d2356
graham <graham.annett@gmail.com>
2024-05-31 13:07:48 -0600

    mixture of depth working for various models

--- src/pretrain_mm/model/adapted/mixture_of_depth.py
+++ src/pretrain_mm/model/adapted/mixture_of_depth.py
@@ -1,7 +1,8 @@
+from functools import partial
 from typing import Any
+
 import torch
 import torch.nn as nn
-from functools import partial
 from transformers import PreTrainedModel
 
 
@@ -18,15 +19,36 @@ class TokenRouter(nn.Module):
 class MixtureOfDepth(nn.Module):
     """
     https://github.com/astramind-ai/Mixture-of-depths/tree/main
+
+    much clearer implementation here:
+        from https://github.com/Mixture-AI/Mixture-of-Depths/blob/master/MoD/MoD.py
+    but not sure if it is correct and does not handle attention_mask,position_ids,past_key_value,cache,etc
+
+    things i changed:
+    - casting to device for position_ids and cache_position
+        - this could be avoided if you use this within the models __init__ since the reason it is necessary
+            is because the hooks related to device_map='auto' on transformers are already set
+    - allowing skip_position_ids to be passed in
+        - this avoids errors with models that dont have the recent version of `apply_rotary_pos_emb` where the
+            position_ids used might correspond to indexes that are not in the selected_tokens
+        - i am not sure if this will impact the implementation as the recent llama model does it this way but models
+            that are from before 2024 it seems to be common
     """
 
-    def __init__(self, capacity: float, block: nn.Module):
+    def __init__(
+        self, block: nn.Module, capacity: float = 0.125, skip_position_ids: bool = False, ensure_devices: bool = True
+    ):
         super().__init__()
         self.block = block
         self.capacity = capacity
         self.router = TokenRouter(block.hidden_size)
 
         self.training_step = 0
+        # for some models, the implementation hasnt been updated in awhile and the position_ids being used will cause
+        # an error. Specifically for Fuyu but seems like any model that uses old `apply_rotary_pos_emb` will have this
+        self._skip_position_ids = skip_position_ids
+        # to be taken out if there is a way to fix the hooks on the models when using device_map='auto'
+        self._ensure_devices = ensure_devices
 
     def forward(
         self,
@@ -53,9 +75,12 @@ class MixtureOfDepth(nn.Module):
         cache = None
 
         processed_tokens = torch.zeros_like(x)
+
         for i in range(b):
             current_selected_mask = selected_mask[i]
             selected_tokens = x[i][current_selected_mask]
+            if self._ensure_devices:
+                position_ids = position_ids.to(x.device)  # CAST-PATCH if using apply_mod_to_hf after init
             selected_position_ids = position_ids[i][current_selected_mask].unsqueeze(0)
             if attention_mask is not None:
                 current_causal_mask = attention_mask[i, 0]
@@ -68,6 +93,8 @@ class MixtureOfDepth(nn.Module):
             if selected_tokens.size(0) > 0:
                 # Dynamic cache management
                 if cache_position is not None:
+                    if self._ensure_devices:
+                        cache_position = cache_position.to(x.device)  # CAST-PATCH if using apply_mod_to_hf after init
                     selected_cache_position = cache_position[selected_mask[i]]
                     block_output = self.block(
                         selected_tokens.unsqueeze(0),
@@ -79,25 +106,28 @@ class MixtureOfDepth(nn.Module):
                         cache_position=selected_cache_position,
                         **kwargs,
                     )
-
-                    if len(block_output) == 2:
-                        processed_tokens[i][selected_mask[i]], cache = block_output
-                    else:
-                        processed_tokens[i][selected_mask[i]] = block_output[0]
-
-                    processed_tokens[i][selected_mask[i]] = processed_tokens[i][selected_mask[i]] * weights[i][
-                        selected_mask[i]
-                    ].unsqueeze(-1)
                 else:
-                    processed_tokens[i][selected_mask[i]] = self.block(
+                    # warning: this wont work as is for many models since
+                    # selected_position_ids shape is less than selected_tokens
+                    # not clear what the impact of this is but it allows model to work
+                    block_output = self.block(
                         selected_tokens.unsqueeze(0),
                         attention_mask=current_causal_mask,
-                        position_ids=selected_position_ids,
+                        position_ids=None if self._skip_position_ids else selected_position_ids,
                         past_key_value=past_key_value,
                         output_attentions=output_attentions,
                         use_cache=use_cache,
                         **kwargs,
-                    )[0] * weights[i][selected_mask[i]].unsqueeze(-1)
+                    )
+
+                if len(block_output) == 2:
+                    processed_tokens[i][selected_mask[i]], cache = block_output
+                else:
+                    processed_tokens[i][selected_mask[i]] = block_output[0]
+
+                processed_tokens[i][selected_mask[i]] = processed_tokens[i][selected_mask[i]] * weights[i][
+                    selected_mask[i]
+                ].unsqueeze(-1)
 
         output = processed_tokens + (x * (~selected_mask).unsqueeze(-1).to(x.dtype))
         return (output, cache) if cache is not None else (output,)
@@ -105,15 +135,12 @@ class MixtureOfDepth(nn.Module):
 
 def _mod_func(mod: nn.Module):
     # _archs = "BloomForCausalLM", "FalconMoDForCausalLM"
-    def _settr(mod_obj, layers_field, layers):
-        setattr(mod_obj, layers_field, layers)
-
     if hasattr(mod, "language_model"):  # for multimodal like fuyu models in general
-        ret_mod, ret_settr = mod.language_model.layers, partial(_settr, mod.language_model, "layers")
+        ret_mod, ret_settr = mod.language_model.model.layers, partial(setattr, mod.language_model.model, "layers")
     elif hasattr(mod, "model"):  # for GPT models
-        ret_mod, ret_settr = mod.model.layers, partial(_settr, mod.model, "layers")
+        ret_mod, ret_settr = mod.model.layers, partial(setattr, mod.model, "layers")
     elif hasattr(mod, "transformers"):  # for ??? models
-        ret_mod, ret_settr = mod.transformers.h, partial(_settr, mod.transformers, "h")
+        ret_mod, ret_settr = mod.transformers.h, partial(setattr, mod.transformers, "h")
     else:
         raise ValueError("Model not supported")
     return ret_mod, ret_settr
@@ -122,8 +149,10 @@ def _mod_func(mod: nn.Module):
 def apply_mod_to_hf(
     model: PreTrainedModel,
     enabled: bool = True,
+    skip_position_ids: bool = False,
     mod_helper: callable = _mod_func,
-    set_device: bool = True,
+    update_name: bool = True,
+    device: bool | str = True,
 ) -> PreTrainedModel:
     if not enabled:
         return model
@@ -133,23 +162,28 @@ def apply_mod_to_hf(
 
     for i, layer in enumerate(mod_layers):
         if i % 2 != 0:
-            new_layer = MixtureOfDepth(0.125, layer)
+            new_layer = MixtureOfDepth(layer, 0.125, skip_position_ids=skip_position_ids)
+
+            if device is True:
+                new_layer.to(layer.parameters().__next__().device)
+            elif isinstance(device, str):
+                new_layer.to(device)
+
         else:
             new_layer = layer
-            if set_device:
-                new_layer.to(layer.parameters().__next__().device)
 
         new_layers.append(new_layer)
     mod_settr_func(new_layers)
 
-    class_name = model.__class__.__name__
-    # Insert MoD before the For
-    if "For" in class_name:
-        parts = class_name.split("For", 1)
-        modified_class_name = parts[0] + "MoDFor" + parts[1]
-    else:
-        modified_class_name = "MoD" + class_name  # If it doesn't find any i prepends MoD
+    if update_name:  # idk why they do this?
+        class_name = model.__class__.__name__
+        # Insert MoD before the For
+        if "For" in class_name:
+            parts = class_name.split("For", 1)
+            modified_class_name = parts[0] + "MoDFor" + parts[1]
+        else:
+            modified_class_name = "MoD" + class_name  # If it doesn't find any i prepends MoD
 
-    model.__class__.__name__ = modified_class_name
+        model.__class__.__name__ = modified_class_name
 
     return model
--- tests/models/test_mixture_of_depth.py
+++ tests/models/test_mixture_of_depth.py
@@ -1,9 +1,11 @@
 import unittest
 
 import torch
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoTokenizer
 
+from pretrain_mm.model import fuyu, paligemma
 from pretrain_mm.model.adapted.mixture_of_depth import MixtureOfDepth, apply_mod_to_hf
+from tests.fixtures.fuyu_fixtures import image, input_string
 
 
 class TestMixtureOfDepth(unittest.TestCase):
@@ -37,6 +39,85 @@ class TestApplyModToHf(unittest.TestCase):
     def test_apply_mod_to_hf(self):
         modified_model = apply_mod_to_hf(self.model, enabled=True)
         self.assertEqual(modified_model.__class__.__name__, "MoDGPT2Model")
+
+
+class TestModMM(unittest.TestCase):
+    def setUp(self):
+        self.device_map = "auto"
+
+    def test_apply_mod_to_llama(self):
+        model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+        model = AutoModelForCausalLM.from_pretrained(model_id, device_map=self.device_map)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        inputs = tokenizer(text=input_string, return_tensors="pt")
+
+        model = apply_mod_to_hf(model)
+        inputs.to(model.device)
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        self.assertTrue(hasattr(outputs, "logits"))
+
+    def test_apply_mod_to_persimmon(self):
+        # for testing locally, we need to use this config
+        model_id = "adept/persimmon-8b-base"
+        model_config = AutoConfig.from_pretrained(
+            model_id,
+            hidden_size=512,
+            intermediate_size=512,
+            num_hidden_layers=4,
+            num_attention_heads=4,
+        )
+
+        model = AutoModelForCausalLM.from_config(model_config)
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        inputs = tokenizer(text=input_string, return_tensors="pt")
+        inputs.to(model.device)
+
+        model = apply_mod_to_hf(model, skip_position_ids=True)
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+        self.assertTrue(hasattr(outputs, "logits"))
+        self.assertEqual(model.__class__.__name__, "PersimmonMoDForCausalLM")
+
+    def test_apply_mod_to_fuyu(self):
+        # need to drop layers for testing locally, i think we need 2 at least for MoD to work
+        model_config = fuyu.FuyuConfig.from_pretrained(fuyu.MODEL_ID).patch(num_hidden_layers=2)
+
+        model = fuyu.FuyuForCausalLM.from_pretrained(fuyu.MODEL_ID, device_map=self.device_map, config=model_config)
+
+        model = apply_mod_to_hf(model)
+        self.assertEqual(model.__class__.__name__, "FuyuMoDForCausalLM")
+        processor = fuyu.FuyuProcessor.from_pretrained(fuyu.MODEL_ID)
+
+        inputs = processor(text=input_string, images=image)
+        inputs.to(model.device)
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        self.assertTrue(hasattr(outputs, "logits"))
+
+    def test_apply_mod_to_gemma(self):
+        model = paligemma.PaliGemmaForConditionalGeneration.from_pretrained(
+            paligemma.MODEL_ID, device_map=self.device_map
+        )
+
+        modified_model = apply_mod_to_hf(model)
+        self.assertEqual(modified_model.__class__.__name__, "PaliGemmaMoDForConditionalGeneration")
+
+        processor = paligemma.PaliGemmaProcessor.from_pretrained(paligemma.MODEL_ID)
+
+        inputs = processor(text=input_string, images=image)
+        inputs.to(model.device)
+
+        with torch.no_grad():
+            outputs = model(**inputs)
+
+        self.assertTrue(hasattr(outputs, "logits"))
 
 
 if __name__ == "__main__":
