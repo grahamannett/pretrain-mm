1236ef2b42dd2b817cfd0cfe7678bc704eb2c992
graham <graham.annett@gmail.com>
2024-05-31 23:48:50 -0600

    commit @ 2405312348

--- src/pretrain_mm/model/adapted/mixture_of_depth.py
+++ src/pretrain_mm/model/adapted/mixture_of_depth.py
@@ -6,16 +6,6 @@ import torch.nn as nn
 from transformers import PreTrainedModel
 
 
-class TokenRouter(nn.Module):
-    def __init__(self, embed_dim):
-        super().__init__()
-        self.weight_predictor = nn.Linear(embed_dim, 1)
-
-    def forward(self, x):
-        weights = self.weight_predictor(x).squeeze(-1)  # [batch_size, seq_len]
-        return weights
-
-
 def _verify_selected_causal_mask(attention_mask, selected_mask):
     causal_masks = []
     for i in range(selected_mask.shape[0]):
@@ -32,60 +22,45 @@ def _ensure_devices(should, device, *args):
 
 
 class MixtureOfDepth(nn.Module):
-    """
-    https://github.com/astramind-ai/Mixture-of-depths/tree/main
-
-    other implementation here:
-        from https://github.com/kyegomez/Mixture-of-Depths/blob/main/mixture_of_depths/main.py
-            - this one seems maybe like it is good since it uses gather/scatter but im not entirely
-                sure if it is correct
-        from https://github.com/Mixture-AI/Mixture-of-Depths/blob/master/MoD/MoD.py
-    but not sure if it is correct and does not handle attention_mask,position_ids,past_key_value,cache,etc
-
-    things i changed:
-    - casting to device for position_ids and cache_position
-        - this could be avoided if you use this within the models __init__ since the reason it is necessary
-            is because the hooks related to device_map='auto' on transformers are already set
-    - allowing skip_position_ids to be passed in
-        - this avoids errors with models that dont have the recent version of `apply_rotary_pos_emb` where the
-            position_ids used might correspond to indexes that are not in the selected_tokens
-        - i am not sure if this will impact the implementation as the recent llama model does it this way but models
-            that are from before 2024 it seems to be common
-    """
+    """ """
 
     def __init__(
         self,
         block: nn.Module,
         capacity: float = 0.125,
+        smooth_interval: int = 1000,
         skip_position_ids: bool = False,
         ensure_devices: bool = True,
         bias: bool = False,
     ):
         super().__init__()
         self.block = block
-        self.capacity = capacity
+
+        # idk
+        self._capacity = capacity
+        self._smooth_interval = smooth_interval
+        self._train_step = 0
 
         self.router = nn.Linear(block.hidden_size, 1, bias=bias)
 
-        self.training_step = 0
+        # to be taken out if there is a way to fix the hooks on the models when using device_map='auto' after init
+        self._ensure_devices = ensure_devices
         # for some models, the implementation hasnt been updated in awhile and the position_ids being used will cause
         # an error. Specifically for Fuyu but seems like any model that uses old `apply_rotary_pos_emb` will have this
         self._skip_position_ids = skip_position_ids
-        # to be taken out if there is a way to fix the hooks on the models when using device_map='auto'
-        self._ensure_devices = ensure_devices
 
-    def forward(self, *args, **kwargs):
-        return self._batch_forward(*args, **kwargs)
-
-    def ensure_device_(self, device, *args):
+    def _check_device(self, device, *args):
         # CAST-PATCH if using apply_mod_to_hf after init
         if self._ensure_devices:
             return [arg.to(device) if arg is not None else None for arg in args]
         return args
 
-    def _batch_forward(
+    def _make_selected_attention_mask(self, attention_mask, selected_mask):
+        return current_causal_mask, mask_s
+
+    def forward(
         self,
-        x: torch.Tensor,
+        hidden_states: torch.Tensor,
         attention_mask: torch.Tensor,
         position_ids: torch.Tensor,
         past_key_value: tuple[torch.Tensor, torch.Tensor],
@@ -94,23 +69,24 @@ class MixtureOfDepth(nn.Module):
         cache_position: torch.Tensor = None,
         **kwargs: Any,
     ) -> tuple[torch.FloatTensor, tuple[torch.FloatTensor, torch.FloatTensor]]:
-        if self.router.training:
-            self.training_step += 1 if self.training_step < 1000 else 999  # wtf is this from?
-            self.capacity = 0.125 + ((1 - 0.125) * (1.0 / self.training_step))  # wtf is this from?
+        if self.router.training and self._train_step < self._smooth_interval:
+            # this "smooths" the capacity from 1.0 to self.capacity_end over the first 1000 steps
+            capa = self.capacity + ((1 - self.capacity) * (1.0 / self.training_step))
+            self._train_step += 1
 
-        b, s, d = x.shape
-        position_ids, cache_position = self.ensure_device_(x.device, position_ids, cache_position)
+        b, s, d = hidden_states.shape
+        position_ids, cache_position = self._check_device(hidden_states.device, position_ids, cache_position)
 
-        weights = self.router(x).squeeze(-1)
+        weights = self.router(hidden_states).squeeze(-1)
 
-        k = int(self.capacity * s)
-        top_k_values, k_indexes = torch.topk(weights, k, dim=1, sorted=True)
-        threshold = top_k_values[:, -1]  # what is this even?
+        k = int(capa * s)
+        top_k_values, k_idxs = torch.topk(weights, k, dim=1, sorted=True)
+        threshold = top_k_values[:, -1]
         selected_mask = weights > threshold.unsqueeze(-1)
 
         cache = None
 
-        output = torch.zeros_like(x)
+        output = torch.zeros_like(hidden_states)
         # make sure that the number of tokens selected is the same for all the batches
         if selected_mask.sum(1).unique(return_counts=True)[1].shape[0] != 1:
             raise ValueError("The number of tokens selected is not the same for all the batches")
@@ -119,7 +95,7 @@ class MixtureOfDepth(nn.Module):
         if (position_ids.ndim == 2) and (position_ids.shape[0] != 1):
             raise ValueError("The position_ids should have a batch dimension of 1?")
 
-        selected_tokens = x[selected_mask].reshape(b, -1, d)
+        selected_tokens = hidden_states[selected_mask].reshape(b, -1, d)
 
         # you want to use masked_select for position_ids/cache_position as they are likely 1D but we need
         # the batch dimension to be the same as the selected_tokens
@@ -146,7 +122,6 @@ class MixtureOfDepth(nn.Module):
             # I believe i verified all of this but spent way too long figuring this bit out.  very frustrating
             # to make sure in future, im leaving the `_verify_selected_causal_mask(attention_mask, selected_mask)` which
             # i am more sure is correct and that should equal current_causal_mask
-
             if mask_s != selected_tokens.shape[1]:
                 raise ValueError("The attention mask should have the same number of tokens as the selected tokens")
         else:
@@ -173,8 +148,8 @@ class MixtureOfDepth(nn.Module):
                 **kwargs,
             )
 
-        if len(hidden_states) == 2:
-            hidden_states, cache = hidden_states
+            if len(hidden_states) == 2:
+                hidden_states, cache = hidden_states
 
         # feel like the better way to do this is use scatter_add but then you need indexes as int, and I am frankly
         # kind of confused by how the indexing of this stuff works when you get to 4D tensors.
@@ -182,94 +157,17 @@ class MixtureOfDepth(nn.Module):
         # confused how exactly to do that operation and seems maybe not ideal if i need to repeat
         # the indexes along the embed dim
         output[selected_mask] = (hidden_states * weights[selected_mask].view(b, -1, 1)).view(-1, d)
-        output = output + (x * (~selected_mask[..., None]).to(x.dtype))
-        return (output, cache) if cache is not None else (output,)
+        output = output + (hidden_states * (~selected_mask[..., None]).to(hidden_states.dtype))
 
-    def _single_forward(
-        self,
-        x: torch.Tensor,
-        attention_mask: torch.Tensor,
-        position_ids: torch.Tensor,
-        past_key_value: tuple[torch.Tensor, torch.Tensor],
-        output_attentions: bool,
-        use_cache: bool,
-        cache_position: torch.Tensor = None,
-        **kwargs: Any,
-    ) -> tuple[torch.FloatTensor, tuple[torch.FloatTensor, torch.FloatTensor]]:
-        b, s, d = x.shape
-        position_ids, cache_position = self.ensure_device_(x.device, position_ids, cache_position)
-        weights = self.router(x)
+        output = (output,)
 
-        if self.router.training:
-            self.training_step += 1 if self.training_step < 1000 else 999
-            self.capacity = 0.125 + ((1 - 0.125) * (1.0 / self.training_step))
+        if cache:
+            output += (cache,)
 
-        k = int(self.capacity * s)
-        top_k_values, _ = torch.topk(weights, k, dim=1, sorted=True)
-        threshold = top_k_values[:, -1]
-        selected_mask = weights > threshold.unsqueeze(-1)
-        cache = None
-
-        processed_tokens = torch.zeros_like(x)
-
-        for i in range(b):
-            current_selected_mask = selected_mask[i]
-            selected_tokens = x[i][current_selected_mask]
-
-            selected_position_ids = position_ids[i][current_selected_mask].unsqueeze(0)
-            if attention_mask is not None:
-                current_causal_mask = attention_mask[i, 0]
-                current_causal_mask = (
-                    current_causal_mask[current_selected_mask][:, current_selected_mask].unsqueeze(0).unsqueeze(0)
-                )  # first if for the one second is for the bs
-            else:
-                current_causal_mask = None
-
-            if selected_tokens.size(0) > 0:
-                # Dynamic cache management
-                if cache_position is not None:
-                    if self._ensure_devices:
-                        cache_position = cache_position.to(x.device)  # CAST-PATCH if using apply_mod_to_hf after init
-                    selected_cache_position = cache_position[selected_mask[i]]
-                    block_output = self.block(
-                        selected_tokens.unsqueeze(0),
-                        attention_mask=current_causal_mask,
-                        position_ids=selected_position_ids,
-                        past_key_value=past_key_value,
-                        output_attentions=output_attentions,
-                        use_cache=use_cache,
-                        cache_position=selected_cache_position,
-                        **kwargs,
-                    )
-                else:
-                    # warning: this wont work as is for many models since
-                    # selected_position_ids shape is less than selected_tokens
-                    # not clear what the impact of this is but it allows model to work
-                    block_output = self.block(
-                        selected_tokens.unsqueeze(0),
-                        attention_mask=current_causal_mask,
-                        position_ids=None if self._skip_position_ids else selected_position_ids,
-                        past_key_value=past_key_value,
-                        output_attentions=output_attentions,
-                        use_cache=use_cache,
-                        **kwargs,
-                    )
-
-                if len(block_output) == 2:
-                    processed_tokens[i][selected_mask[i]], cache = block_output
-                else:
-                    processed_tokens[i][selected_mask[i]] = block_output[0]
-
-                processed_tokens[i][selected_mask[i]] = processed_tokens[i][selected_mask[i]] * weights[i][
-                    selected_mask[i]
-                ].unsqueeze(-1)
-
-        output = processed_tokens + (x * (~selected_mask).unsqueeze(-1).to(x.dtype))
-        return (output, cache) if cache is not None else (output,)
+        return output
 
 
 def _mod_func(mod: nn.Module):
-    # _archs = "BloomForCausalLM", "FalconMoDForCausalLM"
     if hasattr(mod, "language_model"):  # for multimodal like fuyu models in general
         ret_mod, ret_settr = mod.language_model.model.layers, partial(setattr, mod.language_model.model, "layers")
     elif hasattr(mod, "model"):  # for GPT models
@@ -281,7 +179,7 @@ def _mod_func(mod: nn.Module):
     return ret_mod, ret_settr
 
 
-def apply_mod_to_hf(
+def convert_hf_model(
     model: PreTrainedModel,
     enabled: bool = True,
     skip_position_ids: bool = False,
@@ -310,7 +208,7 @@ def apply_mod_to_hf(
         new_layers.append(new_layer)
     mod_settr_func(new_layers)
 
-    if update_name:  # idk why they do this?
+    if update_name:
         class_name = model.__class__.__name__
         # Insert MoD before the For
         if "For" in class_name:
