3994f6db0eb3b46f8d532402cb37d013c8bfdd05
graham <graham.annett@gmail.com>
2024-07-31 21:47:27 -0600

    add utils from other repo

--- src/pretrain_mm/datasets/rico/rico_utils.py
+++ src/pretrain_mm/datasets/rico/rico_utils.py
@@ -67,3 +67,18 @@ def parse_ui_elements(text):
             )
 
     return ui_elements
+
+
+if __name__ == "__main__":
+    import json
+
+    # Example usage
+    input_str = """
+    TOOLBAR 0 999 31 109 (TEXT Inbox 40 158 54 85, PICTOGRAM a white speech bubble with a plus sign on a blue background . 671 789 39 102, PICTOGRAM a magnifying glass icon on a blue background . 787 906 39 101, PICTOGRAM three white circles are lined up on a blue background . 908 999 39 101), BUTTON CHATROOMS 0 333 108 171, NAVIGATION_BAR 0 998 109 171 (BUTTON DIRECT 331 664 109 171, BUTTON CONTACTS 672 998 110 173), PICTOGRAM a green speech bubble with two white circles inside of it is in a yellow circle . 29 166 184 260, TEXT Team Inbox 193 409 194 222, PICTOGRAM three yellow stars are shining on a white background . 412 462 193 223, TEXT You: 199 267 226 251, TEXT Picture Message 273 539 226 252, TEXT now 904 965 202 222, PICTOGRAM check 903 962 228 254, TEXT Inbox is more fun with friends! 258 736 319 345, BUTTON Invite Friends 391 605 346 371
+    """
+
+    parsed_ui = parse_ui_elements(input_str)
+
+    # Convert to JSON
+    json_output = json.dumps(parsed_ui, indent=2)
+    print(json_output)
